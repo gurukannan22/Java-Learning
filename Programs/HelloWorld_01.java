1a60e0cc0113ef09979ed551ae9c114e0db83fe8
Guru Kannan <gurukannan2000@gmail.com>
2025-03-06 13:50:37 +0530

    HelloWorld.java

---
+++ Programs/HelloWorld_01.java
@@ -0,0 +1,5 @@
+public class HelloWorld_01 {
+    public static void main(String[] args) { // Main Method => Entry Point of the java program used to run the java file
+        System.out.println("Hello World!"); // Print Statement
+    }
+}
