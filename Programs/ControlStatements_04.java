a1ace142be376471f9481f034dd0df28dc826469
Guru Kannan <gurukannan2000@gmail.com>
2025-03-06 15:02:14 +0530

    Control and Looping Statements

--- Programs/ControlStatements_04.java
+++ Programs/ControlStatements_04.java
@@ -1,15 +1,15 @@
 public class ControlStatements_04 {
     public static void main(String[] args) {
-       IfElseExample.ifElse();
+       IfElseExample.ifElse(); // Method calling 
        SwitchExample.switchStatement();
 
     }
 
     class IfElseExample{
-        static void ifElse() {
+        static void ifElse() { // Method returns nothing (void => returns nothing) 
             int a = 10, b = 20, sum;
             sum = a+b;
-            if(sum < 40){
+            if(sum < 40){ // checks the the given statement is true
                 System.out.println("The Sum is Lesser than 40");
             }else{
                 System.out.println("The Sum is Greater than 40");
@@ -20,13 +20,13 @@ public class ControlStatements_04 {
     class SwitchExample{
         static void switchStatement(){
             int day = 5;
-            switch (day) {
+            switch (day) { // Switch based the key 
                 case 1:
                     System.out.println("Monday");
                     break;
                 case 2:
                     System.out.println("Tuesday");
-                    break;
+                    break; 
                 case 3:
                     System.out.println("Wednesday");
                     break;
