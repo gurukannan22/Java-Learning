02f80db11ad6173fb103040ca68a0f54f5f70219
Guru Kannan <gurukannan2000@gmail.com>
2025-04-02 20:12:37 +0530

    commented - JSP codes to avoid run issues

--- JSP/Programs/MyWebApp/src/main/java/com/ednue/AddServlet.java
+++ JSP/Programs/MyWebApp/src/main/java/com/ednue/AddServlet.java
@@ -1,23 +1,23 @@
-package com.ednue;
+// package com.ednue;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+// import java.io.IOException;
+// import java.io.PrintWriter;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
 
-public class AddServlet extends HttpServlet{
+// public class AddServlet extends HttpServlet{
 	
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		int first = Integer.parseInt(req.getParameter("num1"));
-		int second = Integer.parseInt(req.getParameter("num2"));
-		int sum = first + second;
+// 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+// 		int first = Integer.parseInt(req.getParameter("num1"));
+// 		int second = Integer.parseInt(req.getParameter("num2"));
+// 		int sum = first + second;
 		
-		PrintWriter out = resp.getWriter();
+// 		PrintWriter out = resp.getWriter();
 		
-		out.println("The sum is : " + sum);
+// 		out.println("The sum is : " + sum);
 		
-	}
+// 	}
 
-}
+// }
